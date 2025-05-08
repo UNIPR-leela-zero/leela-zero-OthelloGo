4f3a7686e78f4e19015a5de1e012f6e90bf6028a
ghizzo03 <richi.ghizzoni@gmail.com>
2025-05-08 15:49:57 +0200

    Riadattamento github prof

--- Go_Othello/build-go/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/riccardoghizzoni/Desktop/leela-zero-unito-finito/gtest
-# Build directory: /Users/riccardoghizzoni/Desktop/leela-zero-unito-finito/build-go/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- Go_Othello/src/GameType.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-enum class GameType {
-    GO,
-    OTHELLO
-};
-
-extern GameType CURRENT_GAME;
